247aae47c920372062b1e7ebf34888feb397fba0
Camilo Rodriguez ingeniero <camilorodriguez2831@gmail.com>
2026-04-22 17:05:43 -0500

    arreglo de la pagina desde fuente

--- Contactos_pagina/carrito.js
+++
@@ -1,9 +0,0 @@
-// carrito.js
-console.log("carrito.js cargado ✅");
-
-function toggleCart() {
-  const panel = document.getElementById('cart-panel');
-  const overlay = document.getElementById('cart-overlay');
-  panel.classList.toggle('open');
-  overlay.classList.toggle('open');
-}
--- Contactos_pagina/interaccion.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.logo').addEventListener('click', function(e) {
-  e.preventDefault();
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
-  });
-});
